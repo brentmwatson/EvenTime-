cf26566de67fe7f899dda2e6564d20a54aee470d
Young Mi Kim <ymkim23@gmail.com>
2016-11-28 17:11:53 -0500

    chris and collin helped with adding static controller to say hello world

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root to: "home#index"
-  devise_for :users
-  #static routes for React
+  # root to: "home#index"
+  # devise_for :users
+  # static routes for React
   get 'static/index'
   root 'static#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

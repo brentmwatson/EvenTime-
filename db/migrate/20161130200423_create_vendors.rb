37acc4369227c6dbe10f6fcdb09bb2804d720e8a
Young Mi Kim <ymkim23@gmail.com>
2016-11-30 15:50:30 -0500

    delete line

--- db/migrate/20161130200423_create_vendors.rb
+++ db/migrate/20161130200423_create_vendors.rb
@@ -3,7 +3,6 @@ class CreateVendors < ActiveRecord::Migration[5.0]
     create_table :vendors do |t|
       t.string :service
       t.references :agreement, foreign_key: true
-      t.references :milestone, foreign_key: true
       t.references :contact, foreign_key: true
       t.references :address, foreign_key: true
       t.timestamps

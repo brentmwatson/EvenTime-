92e00e7b0b96c241b1adf8e1590340e5663c105e
Brent Watson <brentmwatson@gmail.com>
2016-12-01 17:28:46 -0500

    envoking the power of devise,see if this works.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base # or ActionController::API
                                                      # or ActionController::Metal
-  # ...
-  protect_from_forgery with: :exception
   # acts_as_token_authentication_handler_for User
 
   # Security note: controllers with no-CSRF protection must disable the Devise fallback,

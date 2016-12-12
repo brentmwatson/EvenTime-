06a5e7e9153a9e99dc58f3b9cfadfaa97970439a
Young Mi Kim <ymkim23@gmail.com>
2016-12-12 16:03:12 -0500

    most of checklist done..

--- webpack/components/LeftbarDays.js
+++ webpack/components/LeftbarDays.js
@@ -11,10 +11,14 @@ class LeftbarDays extends React.Component {
   // var b = moment([2007, 0, 28]);
   // a.diff(b, 'days') // 1
   render(){
-    var date = moment().format('L')
-
+    var today = moment()
+    let user = JSON.parse(sessionStorage.getItem('user'))
+    var eventDate = moment(user.events[0].date)
+    var diff = moment(eventDate).diff(today, 'days')
     return (<div>
-        <h5></h5>
+<h4>Your event date!!!!!</h4>
+      <h5>{eventDate.format('L')}</h5>
+        <h5>{diff} days away</h5>
     </div>
     )
   }
--- webpack/components/LeftbarTasks.js
+++ webpack/components/LeftbarTasks.js
@@ -8,11 +8,28 @@ class Leftbar extends React.Component {
   constructor(props){
     super(props)
   }
+// componentWillMount(){
+//   let user = JSON.parse(sessionStorage.getItem('user'))
+//
+//   fetch('/api/events/' + user.events[0].id + '?user_token=' +  sessionStorage.getItem('auth_token') + '&user_email=' + sessionStorage.getItem('email'),
+//       {
+//       method: 'GET',
+//       headers: {
+//           'Content-Type': 'application/json'
+//       }
+//       })
+//       .then(response => response.json())
+//       // .then(response => console.log(response))
+//       .then(response => this.setState({milestones: response.event.milestones}))
+// }
   render(){
-
+    // var checklistItemList = this.state.milestones.filter((milestone, i) =>{
+    //         return <ChecklistItem completed/>})
+            //.length
     return (
       <div>
                 <h5>3 out of 100 tasks done</h5>
+                {/* <h5>{checklistItemList.length}</h5> */}
       </div>
     )
   }

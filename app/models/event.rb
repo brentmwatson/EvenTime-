82546b04c68ffb60e1b3b0e6f30445681bbf7121
Brent Watson <brentmwatson@gmail.com>
2016-12-14 13:53:43 -0500

    added serializer and relationship for vendor fix

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :user
   has_many :milestones
-  
+  has_many :vendors, :through => :milestones
 end
--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,4 +1,5 @@
 class EventSerializer < ActiveModel::Serializer
   attributes :id, :theme, :date
   has_many :milestones
+  has_many :vendors
 end

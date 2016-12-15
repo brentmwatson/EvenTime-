340e6971cf93a25ec6ad8ced84b88f130d2d7d05
Young Mi Kim <ymkim23@gmail.com>
2016-12-15 13:27:07 -0500

    styling this  ugly thing

--- webpack/components/VendorSingle.js
+++ webpack/components/VendorSingle.js
@@ -6,24 +6,24 @@ class VendorSingle extends React.Component {
     constructor(props){
         super(props)
         this.deleteItem = this.deleteItem.bind(this)
-        this.updateItem = this.updateItem.bind(this)
+        // this.updateItem = this.updateItem.bind(this)
         this.state = {
             vendor: props.vendor,
         }
     }
-    updateItem() {
-        fetch('/api/vendors/' + this.state.vendor.id + '?user_token=' +  sessionStorage.getItem('auth_token') + '&user_email=' + sessionStorage.getItem('email'),
-        {
-            method: 'put',
-            body: JSON.stringify(this.state.vendor),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-        .then(response => response.json())
-        //.then(response => console.log(response))
-        .then(response => window.location.reload())
-    }
+    // updateItem() {
+    //     fetch('/api/vendors/' + this.state.vendor.id + '?user_token=' +  sessionStorage.getItem('auth_token') + '&user_email=' + sessionStorage.getItem('email'),
+    //     {
+    //         method: 'put',
+    //         body: JSON.stringify(this.state.vendor),
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         }
+    //     })
+    //     .then(response => response.json())
+    //     //.then(response => console.log(response))
+    //     .then(response => window.location.reload())
+    // }
     deleteItem() {
         fetch('/api/vendors/' + this.state.vendor.id + '?user_token=' +  sessionStorage.getItem('auth_token') + '&user_email=' + sessionStorage.getItem('email'),
         {
@@ -37,38 +37,29 @@ class VendorSingle extends React.Component {
             <div className="panel panel-default">
                 <div className="panel-body">
                     <div className="row">
-                        <div className="col-sm-4 text-left">
-                            <h3>{this.state.vendor.contact.name}</h3>
-                        </div>
-                        <div className="col-sm-4 text-center">
-                            <h3>{this.state.vendor.contact.role}</h3>
-                        </div>
-                        <div className="col-sm-4 text-center">
-                            <h3>{this.state.vendor.contact.phone_num}</h3>
+                        <div className="col-sm-12 text-center">
+                            <h3>{this.state.vendor.contact.name}  |  {this.state.vendor.contact.role}</h3>
                         </div>
                     </div>
                     <div className="row">
-                        <div className="col-sm-4 text-right">
-                            <h5>{this.state.vendor.address.street}</h5>
-                        </div>
-                        <div className="col-sm-3 text-right">
-                            <h5>{this.state.vendor.address.city},{this.state.vendor.address.state}</h5>
-                        </div>
-                        <div className="col-sm-3 text-left">
-                            <h5>{this.state.vendor.address.zip}</h5>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-sm-4 text-right">
-                            <h5>{this.state.vendor.contact.email}</h5>
+                        <div className="col-sm-6 text-center">
+                            <h5>phone number: {this.state.vendor.contact.phone_num}</h5>
                         </div>
                         <div className="col-sm-6 text-right">
-                            <h5>{this.state.vendor.contact.f_name} {this.state.vendor.contact.l_name}</h5>
+                            <h5>address: {this.state.vendor.address.street},  {this.state.vendor.address.city}, {this.state.vendor.address.state} {this.state.vendor.address.zip}</h5>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-sm-6 text-center">
+                            <h5>email:{this.state.vendor.contact.email}</h5>
+                        </div>
+                        <div className="col-sm-6 text-right">
+                            <h5>contact name:  {this.state.vendor.contact.f_name} {this.state.vendor.contact.l_name}</h5>
                         </div>
                     </div>
                     <div className="row">
                         <div className="col-sm-6 vendorSave">
-                            <button className="btn btn-default saveVBtn" onClick={this.updateItem}>Update</button>
+                            {/* <button className="btn btn-default saveVBtn" onClick={this.updateItem}>Update</button> */}
                         </div>
                         <div className="col-sm-6 text-right vendorDelete">
                             <button className="btn btn-default deleteVBtn" onClick={this.deleteItem}>Delete</button>

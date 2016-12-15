4bcdf85ca4ff96568ef9fd9302fa906c8e795c3e
Young Mi Kim <ymkim23@gmail.com>
2016-12-15 12:04:24 -0500

    adding

--- webpack/components/VendorSingle.js
+++ webpack/components/VendorSingle.js
@@ -21,7 +21,7 @@ class VendorSingle extends React.Component {
             }
         })
         .then(response => response.json())
-         //.then(response => console.log(response))
+        //.then(response => console.log(response))
         .then(response => window.location.reload())
     }
     deleteItem() {
@@ -38,52 +38,47 @@ class VendorSingle extends React.Component {
                 <div className="panel-body">
                     <div className="row">
                         <div className="col-sm-4 text-left">
-                            <h3>{this.state.vendor.name}</h3>
+                            <h3>{this.state.vendor.contact.name}</h3>
                         </div>
                         <div className="col-sm-4 text-center">
-                            <h3>{this.state.vendor.role}</h3>
+                            <h3>{this.state.vendor.contact.role}</h3>
                         </div>
                         <div className="col-sm-4 text-center">
-                            <h3>{this.state.vendor.phone_num}</h3>
-                        </div>
-                    </div>
-                     <div className="row">
-                        <div className="col-sm-4 text-right">
-                            <h5>{this.state.vendor.street}</h5>
-                        </div>
-                        <div className="col-sm-3 text-right">
-                            <h5>{this.state.vendor.city}, {this.state.vendor.state}</h5>
-                        </div>
-                        <div className="col-sm-3 text-left">
-                            <h5>{this.state.vendor.zip}</h5>
+                            <h3>{this.state.vendor.contact.phone_num}</h3>
                         </div>
                     </div>
                     <div className="row">
                         <div className="col-sm-4 text-right">
-                            <h5>{this.state.vendor.email}</h5>
+                            <h5>{this.state.vendor.address.street}</h5>
                         </div>
                         <div className="col-sm-3 text-right">
-                            <h5>{this.state.vendor.f_name}</h5>
+                            <h5>{this.state.vendor.address.city},{this.state.vendor.address.state}</h5>
                         </div>
                         <div className="col-sm-3 text-left">
-                            <h5>{this.state.vendor.l_name}</h5>
+                            <h5>{this.state.vendor.address.zip}</h5>
                         </div>
                     </div>
-                     <div className="row">
-                        <div className="col-sm-6 pull-left">
-                            <div className="form-group"> <button className="btn btn-default pull-right" onClick={this.updateItem}>update</button>
+                    <div className="row">
+                        <div className="col-sm-4 text-right">
+                            <h5>{this.state.vendor.contact.email}</h5>
+                        </div>
+                        <div className="col-sm-6 text-right">
+                            <h5>{this.state.vendor.contact.f_name} {this.state.vendor.contact.l_name}</h5>
                         </div>
                     </div>
-                    <div className="col-sm-6 pull-right">
-                        <div className="form-group"> <button className="btn btn-default pull-right" onClick={this.deleteItem}>delete</button>
+                    <div className="row">
+                        <div className="col-sm-6 vendorSave">
+                            <button className="btn btn-default saveVBtn" onClick={this.updateItem}>Update</button>
+                        </div>
+                        <div className="col-sm-6 text-right vendorDelete">
+                            <button className="btn btn-default deleteVBtn" onClick={this.deleteItem}>Delete</button>
+                        </div>
                     </div>
+                    <hr />
                 </div>
             </div>
-            <hr />
         </div>
-    </div>
-</div>
-)
+    )
 }
 }
 
